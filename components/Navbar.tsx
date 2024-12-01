7e0e843ca604628eb69a17a72f159b0686999788
dangabrielle <124424171+dangabrielle@users.noreply.github.com>
2024-11-30 14:15:39 -1000

    include avatar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,15 @@
+import { Avatar, AvatarImage, AvatarFallback } from "./ui/avatar";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="bg-primary dark:bg-slate-700 text-white px-2 py-2 flex items-center justify-between">
+      <p className="text-center">Navbar</p>
+      <Avatar>
+        <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+        <AvatarFallback className="text-black">BT</AvatarFallback>
+      </Avatar>
+    </div>
+  );
 };
 
 export default Navbar;
